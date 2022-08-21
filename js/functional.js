8f1b4550d7ce49e2a1b83425149cc8d0742dcc12
radagesth-NoteBook <roderickroco@gmail.com>
2022-08-21 12:05:56 +0200

    se hace rollback en la implementacion del js del contador en el js functional

--- js/functional.js
+++ js/functional.js
@@ -11,28 +11,4 @@ btnReset.addEventListener("click", () => {value=0;numContainer.textContent = val
 
 
 
-var btnElm = document.getElementById('btnClick'),
-    btnClear = document.getElementById('btnClear'),
-    pElm = document.getElementById('contarClick')
-    sect = document.getElementById('sect'),
-    contar = 0;
-        
-const sbg = function(){
-    const randColor = Math.floor(Math.random() * 16777215).toString(16);
-    sect.style.backgroundColor = "#" + randColor;
-}
-        
-pElm.textContent = 0;
-        
-btnElm.onclick = function(){
-    contar++;
-    pElm.textContent = contar;
-    sbg();
-}
-        
-btnClear.onclick = function(){
-    pElm.textContent = 0;
-    contar = 0;
-    sect.style.backgroundColor = "white";
-}
 
