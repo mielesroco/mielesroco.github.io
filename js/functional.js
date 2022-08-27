d53e216143fc6dcb1141111639592d90b8201452
radagesth-NoteBook <roderickroco@gmail.com>
2022-08-27 11:29:19 +0200

    se crea la funcion contador que el boton debe ir incrementando a medida que se presiona el boton en pizarra.html

--- js/functional.js
+++ js/functional.js
@@ -3,12 +3,14 @@ let value = 0;
 let btnInc = document.querySelector('.inc');
 let btnDec = document.querySelector('.dec');
 let btnReset = document.querySelector('.reset');
-
+let numContador = 0;
 btnInc.addEventListener("click", () => {value++;numContainer.textContent = value});
 btnDec.addEventListener("click", () => {value--;numContainer.textContent = value});
 btnReset.addEventListener("click", () => {value=0;numContainer.textContent = value});
 
 
 
-
+contar = function(){
+            numContador ++
+        }
 
