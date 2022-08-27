c7e75bc50ec7572cdf6219adca1c48888f5852e7
radagesth-NoteBook <roderickroco@gmail.com>
2022-08-27 11:41:16 +0200

    se cambian variables no usadas en el js , en pizarra html se agrega el uso de localStorage

--- js/functional.js
+++ js/functional.js
@@ -3,12 +3,4 @@ let value = 0;
 let btnInc = document.querySelector('.inc');
 let btnDec = document.querySelector('.dec');
 let btnReset = document.querySelector('.reset');
-let numContador = 0;
-
-
-
-
-contar = function(){
-            numContador ++
-        }
 
